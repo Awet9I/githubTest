c4408382fd9b4ea9bfc33e02edb9f29ea5cbcea2
Awet9I <s362104@oslomet.no>
2021-08-23 14:11:34 +0200

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("\nPrinting from 1 to 10");
+        for(int i = 0; i < 10; i++){
+
+            System.out.println(i);
+        }
+    }
+}

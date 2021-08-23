34e41f736d8300ed0dba6fe9dbf287f1a21c06c3
Awet9I <s362104@oslomet.no>
2021-08-23 14:21:54 +0200

    change from second student, count to 20

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,8 +2,14 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("\nPrinting from 1 to 10");
-        for(int i = 0; i < 10; i++){
+        for (int i = 0; i < 10; i++) {
 
+            System.out.println(i);
+        }
+
+        System.out.println("\n Printing from 10 to 20");
+        for (int i = 10; i < 20; i++)
+        {
             System.out.println(i);
         }
     }
